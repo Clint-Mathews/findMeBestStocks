8fc817b7516fee3fa17af39baa0cb012c6c84e65
“Clint-Mathews” <cmathe17@ford.com>
2023-09-07 22:34:52 +0530

    feat: fixing github pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,6 +46,4 @@ resource "aws_lambda_function" "find_stock_lambda" {
   function_name = "find_stocks_and_update"
   role          = aws_iam_role.iam_for_lambda.arn
   handler  = "find_trending_stocks_to_buy.handler"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
 }
